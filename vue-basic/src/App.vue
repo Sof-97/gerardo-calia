cfc7dc0fa2f5d08fd0973668fdaa86adfb1793cf
Gerardo Calia <sofesso97@gmail.com>
2022-06-19 17:52:13 +0200

    Revert "Safe - Pre Tailwind"
    This reverts commit 293232fe802a9f9e04ac281fcc847ef0fe17cf4c.

--- vue-basic/src/App.vue
+++ vue-basic/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
   <div>
     <header>
       <nav-bar></nav-bar>
@@ -7,21 +8,34 @@
       gigione
     </main>
   </div>
+=======
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
+>>>>>>> parent of 293232f (Safe - Pre Tailwind)
 </template>
 
-<script>
-import NavBar from "./components/NavBar.vue";
-export default {
-  components: { NavBar },
-  name: "App",
-};
-</script>
-
 <style lang="scss">
-header{
-  display: flex;
-  justify-content: end;
-  margin-top: 0rem;
-  margin-right: 3rem;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
--- vue-basic/src/main.js
+++ vue-basic/src/main.js
@@ -1,4 +1,13 @@
+<<<<<<< HEAD
 import { createApp } from 'vue'
 import App from './App.vue'
 import './css/tailwind.css'
 createApp(App).mount('#app')
+=======
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+createApp(App).use(store).use(router).mount("#app");
+>>>>>>> parent of 293232f (Safe - Pre Tailwind)
