8e6b0fbca80ede59b7cd5a994eff7496072e3657
Gerardo Calia <sofesso97@gmail.com>
2022-06-20 18:45:20 +0200

    Pre animation

--- vue-basic/src/App.vue
+++ vue-basic/src/App.vue
@@ -6,21 +6,56 @@
 		<main class="container min-h-full flex justify-center items-center">
 			<h1 class="font-bold text-7xl mt-72 text-emerald-500 ml-8 md:ml-0"><span>Gerardo </span>Calia</h1>
 		</main>
+		<email-comp class="email fixed"></email-comp>
+		<social-comp id="social" class="fixed"></social-comp>
 	</div>
 </template>
 
 <script>
 	import NavBar from "./components/NavBar.vue";
+	import EmailComp from "./components/EmailComp.vue";
+	import SocialComp from "./components/SocialComp.vue";
+
 	export default {
-		components: { NavBar },
+		components: { NavBar, EmailComp, SocialComp },
 		name: "App",
+		data() {
+			return {};
+		},
+		methods: {},
+		mounted() {
+			window.onscroll = function () {
+				let scrollY = document.documentElement.scrollTop;
+				let social = document.getElementById("social");
+				console.log(scrollY);
+				if (scrollY > 10) {
+					social.classList.add("rotate");
+				} else {
+					social.classList.remove("rotate");
+				}
+			};
+		},
 	};
 </script>
 
 <style lang="scss">
-	h1 {
-		span {
-			opacity: 0.7;
+	main {
+		h1 {
+			span {
+				opacity: 0.7;
+			}
 		}
 	}
+	.email {
+		right: -8rem;
+		bottom: 180px;
+		transform: rotate(90deg);
+	}
+	#social {
+		width: fit-content;
+		transition: 1s ease-in-out;
+		bottom: 0px;
+		left: 3rem;
+	}
+	
 </style>
