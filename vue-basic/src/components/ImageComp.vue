37265c9e11bc4eaecc007eca513b1ece749688f1
Gerardo Calia <92599412+Sof-97@users.noreply.github.com>
2022-07-30 11:53:02 +0200

    Update ImageComp.vue

--- vue-basic/src/components/ImageComp.vue
+++ vue-basic/src/components/ImageComp.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="flex justify-center">
 		<img src="../assets/img/CV.jpg" alt="" class="w-52 rounded-t-lg mix-blend-lighten" />
+		<h2>Sito in aggiornamento.</h2>
 	</div>
 </template>
 <script>
