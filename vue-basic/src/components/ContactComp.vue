c184b0a48a8fbf2d66167708247252a590095ae7
Gerardo Calia <92599412+Sof-97@users.noreply.github.com>
2022-08-04 17:35:01 +0200

    ^^

--- vue-basic/src/components/ContactComp.vue
+++ vue-basic/src/components/ContactComp.vue
@@ -1,22 +1,6 @@
 <template>
     <div class="w-10/12 mx-auto">
-        <form name="contact" method="POST" data-netlify="true" netlify>
-            <div class="mt-4">
-                <label>Your Name:</label>
-                <input class="w-full mt-1 p-2" type="text" name="name" />
-            </div>
-            <div class="mt-4">
-                <label>Your Email:</label>
-                <input class="w-full mt-1 p-2" type="email" name="email" />
-            </div>
-            <div class="mt-4">
-                <label>Message:</label>
-                <textarea class="w-full mt-1 p-2" name="message"></textarea>
-            </div>
-            <div class="flex justify-center mt-4">
-                <button class="text-slate-200 py-2 px-4" type="submit">send</button>
-            </div>
-        </form>
+
     </div>
 </template>
 
